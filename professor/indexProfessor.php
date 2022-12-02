6c05b9356317508d94196744d0c1a027155c1e59
viniciusm49 <63761790+viniciusm49@users.noreply.github.com>
2022-12-02 00:56:12 -0300

    indexprofessor

--- professor/indexProfessor.php
+++ professor/indexProfessor.php
@@ -2,13 +2,6 @@
 include('conexao.php');
 
 if(isset($_POST['usuario']) || isset($_POST['senha'])) {
-
-    if(strlen($_POST['usuario']) == 0) {
-        echo "Preencha seu nome de usuario";
-    } else if(strlen($_POST['senha']) == 0) {
-        echo "Preencha sua senha";
-    } else {
-
         $usuario = $mysqli->real_escape_string($_POST['usuario']);
         $senha = $mysqli->real_escape_string($_POST['senha']);
         $sql_code = "SELECT * FROM Professores WHERE usuario = '$usuario' AND senha = '$senha'";
@@ -17,48 +10,9 @@ if(isset($_POST['usuario']) || isset($_POST['senha'])) {
         $quantidade = $sql_query->num_rows;
 
         if($quantidade == 1) {
-            
             $usuario = $sql_query->fetch_assoc();
-
-            if(!isset($_SESSION)) {
-                session_start();
-            }
-
-            $_SESSION['idprofessor'] = $usuario['idprofessor'];
-            $_SESSION['nome'] = $usuario['nome'];
-
-            header("Location: painel.php");
-
+            echo json_encode($usuario);
         } else {
-            echo "Falha ao logar! E-mail ou senha incorretos";
+            echo "errologin";
         }
-
-    }
-
 }
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login - Professor</title>
-</head>
-<body>
-    <h1>Acesse sua conta</h1>
-    <form action="" method="POST">
-        <p>
-            <label>E-mail</label>
-            <input type="text" name="usuario">
-        </p>
-        <p>
-            <label>Senha</label>
-            <input type="password" name="senha">
-        </p>
-        <p>
-            <button type="submit">Entrar</button>
-        </p>
-    </form>
-</body>
-</html>
